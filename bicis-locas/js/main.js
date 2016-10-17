91b33c17b2e3e78e98090e78709d5e63e9f6bb4b
OriannaCorona <oriannalievanos12@gmail.com>
2016-10-17 21:39:21 +0000

    agrego parte del segundo ejercicio, ya esta que los primeros 4 campos y la seleccion de tipo de bici sean obligatorias

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,3 +1,14 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	/* Escribe tú código aquí*/ 
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var mail = document.getElementById("input-email").value;
+	var clave = document.getElementById("input-password").value;
+	if(nombre == "" || apellido == "" || mail == "" || clave == ""){
+		alert("Estos campos son OBLIGATORIOS");
+	}
+	camposSeleccion = document.getElementById("seleccion").selectedIndex;
+	if (camposSeleccion == null || camposSeleccion == 0){
+		alert("Necesitas elegir alguna opción de bicicleta");
+	}
 }
--- traduccion-formulario/js/main.js
+++ traduccion-formulario/js/main.js
@@ -1,29 +1,9 @@
-function cambiaIdioma(idioma){
-	var ingreso = document.getElementById("form-signin-heading");
-	var check = document.getElementById("check");
-	var btnSignIn = document.getElementById("inputSignIn");
-	if (idioma=="es"){
-		ingreso.innerHTML = "Por favor inicia sesión";
-		check.innerHTML = "Recordar datos";
-		btnSignIn.innerHTML = "Iniciar Sesión";
-		document.getElementById("inputEmail").setAttribute("placeholder", "Correo Electrónico");
-		document.getElementById("inputPassword").setAttribute("placeholder", "Contraseña");
+function cambiaIdioma(){
+	document.getElementById("form-signin-heading").innerHTML = "Por favor inicia sesión";
+	document.getElementById("inputEmail").setAttribute("placeholder", "Correo Electrónico");
+	document.getElementById("inputPassword").setAttribute("placeholder", "Contraseña");
+	document.querySelector("span").innerHTML = "Recordar datos";
+	document.querySelector("button").innerHTML = "Iniciar Sesión";
+}
 
-	}else{
-		if (idioma=="en"){
-		ingreso.innerHTML = "Pleace sign in";
-		check.innerHTML = "Remember me";
-		btnSignIn.innerHTML = "Sign in";
-		document.getElementById("inputEmail").setAttribute("placeholder", "Please enter your email");
-		document.getElementById("inputPassword").setAttribute("placeholder", "Password");
-	}
-}
-}
-var btnEs = document.getElementById("btnEs");
-btnEs.onclick = function (){
-	cambiaIdioma("es");
-};
-var btnEn = document.getElementById("btnEn");
-btnEn.onclick = function (){
-	cambiaIdioma("en");
-};
+
